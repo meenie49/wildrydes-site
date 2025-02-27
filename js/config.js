ef0ec5c2c866c7d26a3c235a506e62b11f2154f7
EC2 Default User <ec2-user@ip-172-31-32-29.ap-northeast-2.compute.internal>
2025-02-27 07:41:03 +0000

    cognito practice

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'ap-northeast-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://do5ginglsj.execute-api.ap-northeast-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://le7dfw1hq9.execute-api.ap-northeast-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
